5707af4b09ac057d00df2298439d4cbc248a532f
Patricia <patysiq@yahoo.com.br>
2021-01-30 10:29:22 -0300

    Initial Commit

---
+++ Drawing-Circle/Drawing_CircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_CircleApp.swift
+//  Drawing-Circle
+//
+//  Created by PATRICIA S SIQUEIRA on 30/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_CircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
